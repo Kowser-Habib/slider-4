bd28631179a1621a8d7775a13bffaaa5f662aa0d
Kowser-Habib <kowserhabib1997@gmail.com>
2020-08-29 08:06:12 +0600

    first commit

---
+++ js/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+    /*slider part-1 start*/
+    $('.slider1').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        fade: true,
+        speed: 2000,
+        cssEase: 'linear',
+        autoplaySpeed: 100,
+        dots:false,
+    });
+    /*slider part-1 end*/
+
+});
